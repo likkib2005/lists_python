d0daaa9414658739718c0af35987bbb113f6faec
likitha <likkilikitha214@gmail.com>
2025-07-22 10:46:43 +0530

    Add files via upload

---
+++ nested_list.py
@@ -0,0 +1,8 @@
+list=[10,34,90,[45,67,34],89]
+print(len(list))
+print(list[3])
+print(list[3][2])
+print(list[3][:])
+print(list[3][::2])
+list_2=[10,34,90,["lakshmi","likitha","rithik"],89]
+print(list_2[3])
